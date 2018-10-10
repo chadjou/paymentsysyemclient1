ec90979212328a9aa80fec6c58c07130a4822e42
Unknown <vladislav.kov@gmail.com>
2018-10-10 07:04:46 +0300

    added getstrings from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ConfigService } from "./config/config.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'payment-client';
+  title = "payment-client";
+
+  config: string[];
+
+  constructor(private configService: ConfigService) {}
+
+  showConfig_v1() {
+    this.configService
+      .getConfig()
+      .subscribe((data: string[]) => (this.config = data));
+  }
+
+  onClickMe() {
+    this.showConfig_v1();
+  }
+
+  onClickMe2() {
+    console.log(this.config);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ConfigService } from "./config/config.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [ConfigService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
